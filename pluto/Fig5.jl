92f9da6900c95cd7639842aa27686a13854d5192
cossio <j.cossio.diaz@gmail.com>
2024-07-06 20:55:00 +0200

    util

--- pluto/Fig5.jl
+++ pluto/Fig5.jl
@@ -43,6 +43,9 @@ using NaNStatistics: nanmean
 # ╔═╡ 2665e9c5-2e4f-41ec-a9f7-58b3b96a9ff3
 using NaNStatistics: nanstd
 
+# ╔═╡ e743ce81-29e8-4bac-b22e-79e123db3326
+using NaNStatistics: nancor
+
 # ╔═╡ 9299b1d4-a573-4808-9e26-396c2e04b8cc
 using Random: bitrand
 
@@ -57,6 +60,14 @@ using Statistics: mean
 
 # ╔═╡ d705aa70-ad0e-47aa-a31b-714984d3b65c
 using StatsBase: countmap
+
+# ╔═╡ d443b9aa-7449-4447-983c-18263e61be12
+md"""
+# Imports
+"""
+
+# ╔═╡ 59e3b0b4-c955-4985-bc79-bbc9220539a6
+import PlutoUI
 
 # ╔═╡ dba2cd0a-5edb-4581-970b-c8d7d84cd331
 import CairoMakie
@@ -90,6 +101,14 @@ import SamApp2024
 
 # ╔═╡ 67f6774a-0417-49ba-a7fc-27b6dd543d38
 import StatsBase
+
+# ╔═╡ 18452b0f-3c62-492c-a41b-6a9043ce86b1
+PlutoUI.TableOfContents()
+
+# ╔═╡ 4206fb3f-99c4-4e4b-bbc9-a97891a2f321
+md"""
+# Load data
+"""
 
 # ╔═╡ dcf0701e-3fcc-4199-8285-803305ee38b2
 # load SHAPE data
@@ -203,7 +222,149 @@ nps_reactivities_rep0 = shape_data_rep0.shape_reactivities[nps, seed_seqs, conds
 # ╔═╡ fdb72e46-1410-488a-89b7-815c288277af
 all_reactivities_rep0 = shape_data_rep0.shape_reactivities[:, nat_seqs, conds_sam_rep0];
 
+# ╔═╡ a9b44f72-eec6-4a23-a308-a4fafeac34e8
+_sites = 3:107
+
+# ╔═╡ 0276201f-b846-4573-b28d-e7e756ba0a50
+md"""
+# Figures
+"""
+
 # ╔═╡ 5ef6ad69-e574-4643-912b-db489114995e
+let fig = Makie.Figure()
+	ax = Makie.Axis(
+		fig[1,1], width=300, height=300, xlabel="SHAPE reactivity", ylabel="frequency", xgridvisible=false, ygridvisible=false, xticks=-2:2:6, yticks=0:2, xtrimspine=true, ytrimspine=true
+	)
+	Makie.hist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[bps, nat_seqs, conds_sam_rep0])), normalization=:pdf, bins=-2:0.05:6, color=(:teal, 0.5), gap=-0.01)
+	Makie.hist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[nps, nat_seqs, conds_sam_rep0])), normalization=:pdf, bins=-2:0.05:6, color=(:orange, 0.5), gap=-0.01)
+	Makie.stephist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[bps, nat_seqs, conds_sam_rep0])), label="base paired", normalization=:pdf, bins=-2:0.05:6, linewidth=3, color=:teal)
+	Makie.stephist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[nps, nat_seqs, conds_sam_rep0])), label="not paired", normalization=:pdf, bins=-2:0.05:6, linewidth=3, color=:orange)
+	Makie.xlims!(-2.2, 6)
+	Makie.ylims!(-0.07, 2)
+	#Makie.axislegend(ax, framevisible=false, patchlabelgap=3, position=(-0.02, 1))
+	Makie.axislegend(ax, position=(0.7, 0.2), framevisible=false)
+	Makie.hidespines!(ax, :t, :r)
+	
+	_dummy_ax = Makie.Axis(fig[1,2], width=20, xgridvisible=false, ygridvisible=false)
+	Makie.hidespines!(_dummy_ax, :t, :b, :r, :l)
+	Makie.hidexdecorations!(_dummy_ax)
+	Makie.hideydecorations!(_dummy_ax)
+	
+	ax = Makie.Axis(fig[1,3], width=300, height=300, xlabel="SHAPE reactivity", ylabel="frequency", xgridvisible=false, ygridvisible=false, xticks=-2:2:6, yticks=0:2, xtrimspine=true, ytrimspine=true)
+	Makie.hist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[bps, nat_seqs, conds_sam_rep0])), label="b.p.", normalization=:pdf, bins=-2:0.05:6, color=(:teal, 0.5), gap=-0.01)
+	Makie.hist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[nps, nat_seqs, conds_sam_rep0])), label="n.p.", normalization=:pdf, bins=-2:0.05:6, color=(:orange, 0.5), gap=-0.01)
+	Makie.stephist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[pks, nat_seqs, conds_mg_rep0])), label="p.k.", normalization=:pdf, bins=-2:0.1:6, linewidth=3, color=:black)
+	Makie.xlims!(-2.2, 6)
+	Makie.ylims!(-0.07, 2)
+	Makie.hidespines!(ax, :t, :r, :l)
+	Makie.hideydecorations!(ax)
+	
+	ax = Makie.Axis(fig[1,4], width=300, height=300, xlabel="SHAPE reactivity", xgridvisible=false, ygridvisible=false, xticks=-2:2:6, yticks=0:2, xtrimspine=true, ytrimspine=true)
+	Makie.hist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[bps, nat_seqs, conds_sam_rep0])), normalization=:pdf, bins=-2:0.05:6, color=(:teal, 0.5), gap=-0.01)
+	Makie.hist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[nps, nat_seqs, conds_sam_rep0])), normalization=:pdf, bins=-2:0.05:6, color=(:orange, 0.5), gap=-0.01)
+	Makie.stephist!(ax, filter(x -> -2 < x < 6, vec(shape_data_rep0.shape_reactivities[pks, nat_seqs, conds_sam_rep0])), label="pseudoknot", normalization=:pdf, bins=-2:0.1:6, linewidth=3, color=:black)
+	Makie.xlims!(-2.2, 6)
+	Makie.ylims!(-0.07, 2)
+	Makie.axislegend(ax, position=(0.7, 0.2), framevisible=false)
+	Makie.hidespines!(ax, :t, :r, :l)
+	Makie.hideydecorations!(ax)
+	
+	_xs = _sites
+	
+	ax = Makie.Axis(fig[2,:], width=900, height=150, xticks=5:10:108, yticks=-2:1:1, xgridvisible=false, ygridvisible=false, ylabel="Δreactivity", xtrimspine=true, ytrimspine=true)
+	
+	Makie.band!(ax, _xs, (ΔR_sam_avg_seed - ΔR_sam_std_seed/2)[_xs], (ΔR_sam_avg_seed + ΔR_sam_std_seed/2)[_xs], markersize=5, color=(:gray, 0.25))
+	Makie.lines!(ax, _xs, ΔR_sam_avg_seed[_xs], linewidth=1, color=:gray)
+	Makie.scatter!(ax, _xs, ΔR_sam_avg_seed[_xs], markersize=5, color=:black, label="Natural")
+	
+	Makie.band!(ax, _xs, (ΔR_sam_avg_rbmlo - ΔR_sam_std_rbmlo/2)[_xs], (ΔR_sam_avg_rbmlo + ΔR_sam_std_rbmlo/2)[_xs], markersize=5, color=(:blue, 0.25))
+	Makie.lines!(ax, _xs, ΔR_sam_avg_rbmlo[_xs], linewidth=1, color=:blue)
+	Makie.scatter!(ax, _xs, ΔR_sam_avg_rbmlo[_xs], markersize=5, color=:blue, label="RBM (RBMscore>300)")
+	Makie.axislegend(ax, position=(0.5, 0), framevisible=false, patchlabelgap=-3)
+	Makie.xlims!(1, 108)
+	Makie.hidespines!(ax, :t, :r, :b)
+	Makie.hidexdecorations!(ax)
+	
+	ax = Makie.Axis(fig[3,:], width=900, height=150, xticks=5:10:108, yticks=-2:1:1, xgridvisible=false, ygridvisible=false, xlabel="site", ylabel="Δreactivity", xtrimspine=true, ytrimspine=true)
+	
+	Makie.band!(ax, _xs, (ΔR_sam_avg_seed - ΔR_sam_std_seed/2)[_xs], (ΔR_sam_avg_seed + ΔR_sam_std_seed/2)[_xs], markersize=5, color=(:gray, 0.25))
+	Makie.lines!(ax, _xs, ΔR_sam_avg_seed[_xs], linewidth=1, color=:gray)
+	Makie.scatter!(ax, _xs, ΔR_sam_avg_seed[_xs], markersize=5, color=:black, label="Natural")
+	
+	Makie.band!(ax, _xs, (ΔR_sam_avg_inf - ΔR_sam_std_inf/2)[_xs], (ΔR_sam_avg_inf + ΔR_sam_std_inf/2)[_xs], markersize=5, color=(:red, 0.25))
+	Makie.lines!(ax, _xs, ΔR_sam_avg_inf[_xs], linewidth=1, color=:red)
+	Makie.scatter!(ax, _xs, ΔR_sam_avg_inf[_xs], markersize=5, color=:red, label="rCM")
+	Makie.axislegend(ax, position=(0.5, 0), framevisible=false, patchlabelgap=-3)
+	Makie.hidespines!(ax, :t, :r)
+	Makie.xlims!(1, 108)
+	
+	# Makie.Label(fig[1,1][1,1,Makie.TopLeft()], "A)", font=:bold, padding=(0,0,10,10))
+	# Makie.Label(fig[1,2][1,1,Makie.TopLeft()], "B)", font=:bold, padding=(0,0,10,10))
+	# Makie.Label(fig[1,3][1,1,Makie.TopLeft()], "C)", font=:bold, padding=(0,0,10,10))
+	# Makie.Label(fig[2,:][1,1,Makie.TopLeft()], "D)", font=:bold, padding=(0,0,0,0))
+	# Makie.Label(fig[3,:][1,1,Makie.TopLeft()], "E)", font=:bold, padding=(0,0,0,0))
+	
+	Makie.resize_to_layout!(fig)
+	#Makie.save("/workspaces/SamApp.jl/notebooks/2024-03-14 New paper figures/Figures/SHAPE reactivities.pdf", fig)
+	fig
+end
+
+# ╔═╡ 94019df5-2de3-4d18-aa73-4d6b1537b835
+md"""
+# Significance of correlation difference by bootstrapping
+
+Following https://stats.stackexchange.com/questions/278751/how-do-i-determine-whether-two-correlations-are-significantly-different.
+"""
+
+# ╔═╡ 1eced2e8-6c9c-4482-8c5d-6f0212ca1bde
+cor(ΔR_sam_avg_seed[_sites], ΔR_sam_avg_inf[_sites])
+
+# ╔═╡ 946c2fb9-ed2d-40bd-8942-38c21d6d38e5
+cor(ΔR_sam_avg_seed[_sites], ΔR_sam_avg_rbmlo[_sites])
+
+# ╔═╡ 1c183be2-7c10-4ddb-b9df-5b8216de3768
+let fig = Makie.Figure()
+	N = 50000
+	resampled_cor_inf = [cor(nanmean(ΔR_sam[_sites, rand(seed_seqs, length(seed_seqs))]; dim=2), nanmean(ΔR_sam[_sites, rand(inf_seqs, length(inf_seqs))]; dim=2)) for _ = 1:N]
+	resampled_cor_rbm = [cor(nanmean(ΔR_sam[_sites, rand(seed_seqs, length(seed_seqs))]; dim=2), nanmean(ΔR_sam[_sites, rand(_rbmlo, length(_rbmlo))]; dim=2)) for _ = 1:N]
+
+	bins = -0.2:0.01:1
+	
+	ax = Makie.Axis(fig[1,1]; width=300, height=300, xgridvisible=false, ygridvisible=false, xlabel="Bootstrapped correlation")
+	Makie.hist!(ax, resampled_cor_inf; normalization=:pdf, color=(:red, 0.5), linewidth=2, bins)
+	Makie.hist!(ax, resampled_cor_rbm; normalization=:pdf, color=(:blue, 0.5), linewidth=2, bins)
+	Makie.stephist!(ax, resampled_cor_inf; normalization=:pdf, label="CM", color=:red, linewidth=2, bins)
+	Makie.stephist!(ax, resampled_cor_rbm; normalization=:pdf, label="RBM", color=:blue, linewidth=2, bins)
+	Makie.xlims!(ax, -0.2, 1)
+	Makie.ylims!(ax, 0, 12)
+	Makie.axislegend(ax; position=:lt, framevisible=false)
+
+	ax = Makie.Axis(fig[1,2]; width=300, height=300, xgridvisible=false, ygridvisible=false, xlabel="Bootstrapped correlation difference (RBM - CM)")
+	Makie.hist!(ax, resampled_cor_rbm - resampled_cor_inf; normalization=:pdf, color=(:black, 0.5), linewidth=2, bins)
+	Makie.stephist!(ax, resampled_cor_rbm - resampled_cor_inf; normalization=:pdf, color=:black, linewidth=2, bins)
+	Makie.xlims!(ax, -0.2, 1)
+	Makie.ylims!(ax, 0, 12)
+
+	Makie.resize_to_layout!(fig)
+	fig
+end
+
+# ╔═╡ e9a343df-5c1c-4650-ae6d-482ed24af863
+let N = 1000000
+	resampled_cor_inf = [cor(nanmean(ΔR_sam[_sites, rand(seed_seqs, length(seed_seqs))]; dim=2), nanmean(ΔR_sam[_sites, rand(inf_seqs, length(inf_seqs))]; dim=2)) for _ = 1:N]
+	resampled_cor_rbm = [cor(nanmean(ΔR_sam[_sites, rand(seed_seqs, length(seed_seqs))]; dim=2), nanmean(ΔR_sam[_sites, rand(_rbmlo, length(_rbmlo))]; dim=2)) for _ = 1:N]
+	mean(resampled_cor_rbm .< resampled_cor_inf)
+end
+
+# ╔═╡ 65dd700e-8bf0-4c2e-83f3-a2ef6b592255
+1/1000000
+
+# ╔═╡ d492a8ee-ae2a-40a0-bf15-6c466097cd78
+md"""
+# Figure with larger std band
+"""
+
+# ╔═╡ a67aee24-3efb-4d69-bf42-d4103b2487f2
 let fig = Makie.Figure()
 	ax = Makie.Axis(
 		fig[1,1], width=300, height=300, xlabel="SHAPE reactivity", ylabel="frequency", xgridvisible=false, ygridvisible=false, xticks=-2:2:6, yticks=0:2, xtrimspine=true, ytrimspine=true
@@ -246,11 +407,11 @@ let fig = Makie.Figure()
 	
 	ax = Makie.Axis(fig[2,:], width=900, height=150, xticks=5:10:108, yticks=-2:1:1, xgridvisible=false, ygridvisible=false, ylabel="Δreactivity", xtrimspine=true, ytrimspine=true)
 	
-	Makie.band!(ax, _xs, (ΔR_sam_avg_seed - ΔR_sam_std_seed/2)[_xs], (ΔR_sam_avg_seed + ΔR_sam_std_seed/2)[_xs], markersize=5, color=(:gray, 0.25))
+	Makie.band!(ax, _xs, (ΔR_sam_avg_seed - ΔR_sam_std_seed)[_xs], (ΔR_sam_avg_seed + ΔR_sam_std_seed)[_xs], markersize=5, color=(:gray, 0.2))
 	Makie.lines!(ax, _xs, ΔR_sam_avg_seed[_xs], linewidth=1, color=:gray)
 	Makie.scatter!(ax, _xs, ΔR_sam_avg_seed[_xs], markersize=5, color=:black, label="Natural")
 	
-	Makie.band!(ax, _xs, (ΔR_sam_avg_rbmlo - ΔR_sam_std_rbmlo/2)[_xs], (ΔR_sam_avg_rbmlo + ΔR_sam_std_rbmlo/2)[_xs], markersize=5, color=(:blue, 0.25))
+	Makie.band!(ax, _xs, (ΔR_sam_avg_rbmlo - ΔR_sam_std_rbmlo)[_xs], (ΔR_sam_avg_rbmlo + ΔR_sam_std_rbmlo)[_xs], markersize=5, color=(:blue, 0.2))
 	Makie.lines!(ax, _xs, ΔR_sam_avg_rbmlo[_xs], linewidth=1, color=:blue)
 	Makie.scatter!(ax, _xs, ΔR_sam_avg_rbmlo[_xs], markersize=5, color=:blue, label="RBM (RBMscore>300)")
 	Makie.axislegend(ax, position=(0.5, 0), framevisible=false, patchlabelgap=-3)
@@ -260,11 +421,11 @@ let fig = Makie.Figure()
 	
 	ax = Makie.Axis(fig[3,:], width=900, height=150, xticks=5:10:108, yticks=-2:1:1, xgridvisible=false, ygridvisible=false, xlabel="site", ylabel="Δreactivity", xtrimspine=true, ytrimspine=true)
 	
-	Makie.band!(ax, _xs, (ΔR_sam_avg_seed - ΔR_sam_std_seed/2)[_xs], (ΔR_sam_avg_seed + ΔR_sam_std_seed/2)[_xs], markersize=5, color=(:gray, 0.25))
+	Makie.band!(ax, _xs, (ΔR_sam_avg_seed - ΔR_sam_std_seed)[_xs], (ΔR_sam_avg_seed + ΔR_sam_std_seed)[_xs], markersize=5, color=(:gray, 0.2))
 	Makie.lines!(ax, _xs, ΔR_sam_avg_seed[_xs], linewidth=1, color=:gray)
 	Makie.scatter!(ax, _xs, ΔR_sam_avg_seed[_xs], markersize=5, color=:black, label="Natural")
 	
-	Makie.band!(ax, _xs, (ΔR_sam_avg_inf - ΔR_sam_std_inf/2)[_xs], (ΔR_sam_avg_inf + ΔR_sam_std_inf/2)[_xs], markersize=5, color=(:red, 0.25))
+	Makie.band!(ax, _xs, (ΔR_sam_avg_inf - ΔR_sam_std_inf)[_xs], (ΔR_sam_avg_inf + ΔR_sam_std_inf)[_xs], markersize=5, color=(:red, 0.2))
 	Makie.lines!(ax, _xs, ΔR_sam_avg_inf[_xs], linewidth=1, color=:red)
 	Makie.scatter!(ax, _xs, ΔR_sam_avg_inf[_xs], markersize=5, color=:red, label="rCM")
 	Makie.axislegend(ax, position=(0.5, 0), framevisible=false, patchlabelgap=-3)
@@ -283,7 +444,9 @@ let fig = Makie.Figure()
 end
 
 # ╔═╡ Cell order:
+# ╠═d443b9aa-7449-4447-983c-18263e61be12
 # ╠═91f4edd8-290d-4270-83c1-f7c6281e9f68
+# ╠═59e3b0b4-c955-4985-bc79-bbc9220539a6
 # ╠═dba2cd0a-5edb-4581-970b-c8d7d84cd331
 # ╠═90bb26b9-08ca-4de7-a383-f9d2e2c200f1
 # ╠═870f93fb-1969-4654-abd3-6aedf1215cec
@@ -307,11 +470,14 @@ end
 # ╠═bd112693-84c9-4f18-927f-b307319006d0
 # ╠═0c7ec2d1-f6fe-4a95-bc81-e320666d880c
 # ╠═2665e9c5-2e4f-41ec-a9f7-58b3b96a9ff3
+# ╠═e743ce81-29e8-4bac-b22e-79e123db3326
 # ╠═9299b1d4-a573-4808-9e26-396c2e04b8cc
 # ╠═530dbe76-e3a2-4658-ab86-daf6588cf372
 # ╠═0c2a4cd9-a42a-42fc-b85b-46e5987a77cc
 # ╠═8da6f416-1a00-4cc8-a9c0-cdd76ec364b0
 # ╠═d705aa70-ad0e-47aa-a31b-714984d3b65c
+# ╠═18452b0f-3c62-492c-a41b-6a9043ce86b1
+# ╠═4206fb3f-99c4-4e4b-bbc9-a97891a2f321
 # ╠═dcf0701e-3fcc-4199-8285-803305ee38b2
 # ╠═d1387ef6-6497-4f0b-873c-4de5f8aa0715
 # ╠═0297ebf2-0822-4ae0-b4e1-73b8ca1c4452
@@ -346,4 +512,14 @@ end
 # ╠═08a76f04-f5bd-4850-82e4-4c4fcea9dc9a
 # ╠═b0c3959c-fb33-4caf-bd16-96885a0cd302
 # ╠═fdb72e46-1410-488a-89b7-815c288277af
+# ╠═a9b44f72-eec6-4a23-a308-a4fafeac34e8
+# ╠═0276201f-b846-4573-b28d-e7e756ba0a50
 # ╠═5ef6ad69-e574-4643-912b-db489114995e
+# ╠═94019df5-2de3-4d18-aa73-4d6b1537b835
+# ╠═1eced2e8-6c9c-4482-8c5d-6f0212ca1bde
+# ╠═946c2fb9-ed2d-40bd-8942-38c21d6d38e5
+# ╠═1c183be2-7c10-4ddb-b9df-5b8216de3768
+# ╠═e9a343df-5c1c-4650-ae6d-482ed24af863
+# ╠═65dd700e-8bf0-4c2e-83f3-a2ef6b592255
+# ╠═d492a8ee-ae2a-40a0-bf15-6c466097cd78
+# ╠═a67aee24-3efb-4d69-bf42-d4103b2487f2
